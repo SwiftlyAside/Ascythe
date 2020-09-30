5811cb1c4c38b68e1464fdeecc4f7c28873945ba
Swiftly <ivecis@live.co.kr>
2020-09-30 12:24:15 +0900

    Added many classes

---
+++ src/main/java/com/ngsc/takeover/service/AuthService.java
@@ -0,0 +1,45 @@
+package com.ngsc.takeover.service;
+
+import com.ngsc.takeover.dto.RegisterRequest;
+import com.ngsc.takeover.model.User;
+import com.ngsc.takeover.model.VerificationToken;
+import com.ngsc.takeover.repository.UserRepository;
+import com.ngsc.takeover.repository.VerificationTokenRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class AuthService {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final VerificationTokenRepository verificationTokenRepository;
+
+    @Transactional
+    public void signup(RegisterRequest registerRequest) {
+        User user = new User();
+        user.setUsername(registerRequest.getUsername());
+        user.setEmail(registerRequest.getEmail());
+        user.setPassword(passwordEncoder.encode(registerRequest.getPassword()));
+        user.setCreated(Instant.now());
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        generateVerificationToken(user);
+    }
+
+    private String generateVerificationToken(User user) {
+        String token = UUID.randomUUID().toString();
+        VerificationToken verificationToken = new VerificationToken();
+        verificationToken.setToken(token);
+
+        verificationTokenRepository.save(verificationToken);
+        return token;
+    }
+}
